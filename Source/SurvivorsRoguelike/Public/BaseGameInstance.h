17c4c9360125facc19f64be0a71b63ad7783f509
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2023-12-08 15:58:34 +0900

    리소스 마네퀸

--- Source/SurvivorsRoguelike/Public/BaseGameInstance.h
+++ Source/SurvivorsRoguelike/Public/BaseGameInstance.h
@@ -1,14 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "BaseGameInstance.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class SURVIVORSROGUELIKE_API UBaseGameInstance : public UGameInstance
 {
