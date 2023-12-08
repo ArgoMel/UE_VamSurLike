552d2acb471bda0354bd191b96950cd9aabb3f0e
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2023-12-08 16:47:34 +0900

    기본 세팅

---
+++ Source/SurvivorsRoguelike/Public/Character/BasePlayerAnimInstance.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BasePlayerAnimInstance.generated.h"
+
+UCLASS()
+class SURVIVORSROGUELIKE_API UBasePlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
