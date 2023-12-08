858b75d6d733eb3cb1f562229b3bccca4d5d42e9
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2023-12-08 15:46:59 +0900

    머티리얼 추가

--- README.md
+++ README.md
@@ -2,9 +2,4 @@
 
 실행하기 전에 밑에 리소스를 다운받고 실행해야 합니다.
 
-파리곤 : 아고라와 모노리스 환경
-	SampleMap은 삭제해도 됩니다.
-
-Content/KiteDemo/*
-Content/ParagonProps/*
-Content/SampleMap/*
+현재 없음
---
+++ Source/SurvivorsRoguelike.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class SurvivorsRoguelikeTarget : TargetRules
+{
+	public SurvivorsRoguelikeTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "SurvivorsRoguelike" } );
+	}
+}
