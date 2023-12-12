0515c43081caca89b6aaff9abc66e910cbf24de7
WI-Jeong <cpdls23@naver.com>
2023-12-12 15:30:13 +0900

    서버용 스폰포인트&페트롤포인트

--- Source/SurvivorsRoguelike/Private/AI/AIPawn.cpp
+++ Source/SurvivorsRoguelike/Private/AI/AIPawn.cpp
@@ -2,14 +2,13 @@
 
 
 #include "../../Public/AI/AIPawn.h"
+#include "../../Public/AI/DefaultAIController.h"
 
 // Sets default values
 AAIPawn::AAIPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	bReplicates = true;
 
 	mBody = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Body"));
 	mMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
@@ -21,23 +20,67 @@ AAIPawn::AAIPawn()
 
 	mMovement->SetUpdatedComponent(mBody);
 
+	mBody->SetCollisionProfileName(TEXT("AI"));
+
 	mMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+	AIControllerClass = ADefaultAIController::StaticClass();
+
+}
+
+void AAIPawn::SetSpawnPoint(AAISpawnPoint* SpawnPoint)
+{
+	mSpawnPoint = SpawnPoint;
+}
+
+void AAIPawn::SetPatrolPointArray(const TArray<TObjectPtr<class APatrolPoint>>& Array)
+{
+	mPatrolPointArray = Array;
+
+	mPatrolPoint.Add(mSpawnPoint->GetActorLocation());
+
+	for (auto& Point : mPatrolPointArray)
+	{
+		mPatrolPoint.Add(Point->GetActorLocation());
+	}
+}
+
+void AAIPawn::NextPatrol()
+{
+	mPatrolIndex = (mPatrolIndex + 1) % mPatrolPoint.Num();
 }
 
 void AAIPawn::OnConstruction(const FTransform& Transform)
 {
+	Super::OnConstruction(Transform);
 }
 
 // Called when the game starts or when spawned
 void AAIPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (!mPatrolPoint.IsEmpty())
+	{
+		FVector	Loc = GetActorLocation();
+		Loc.Z -= mBody->GetScaledCapsuleHalfHeight();
+
+		mPatrolPoint.Add(Loc);
+
+		for (auto& Point : mPatrolPointArray)
+		{
+			mPatrolPoint.Add(Point->GetActorLocation());
+		}
+	}
 }
 
 void AAIPawn::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
+	Super::EndPlay(EndPlayReason);
+
+	if (IsValid(mSpawnPoint))
+		mSpawnPoint->ClearObject();
 }
 
 // Called every frame
@@ -47,18 +90,9 @@ void AAIPawn::Tick(float DeltaTime)
 
 }
 
-void AAIPawn::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
-{
-}
-
 void AAIPawn::SetCollisionProfile(const FName& Name)
 {
+	mBody->SetCollisionProfileName(Name);
 }
 
-// Called to bind functionality to input
-void AAIPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
