08747b16c6e7f53a3d518cbeb5110b4b90a0e0eb
Twilight-Daisy <65950455+Twilight-Daisy@users.noreply.github.com>
2023-12-15 14:20:48 +0900

    캐릭터 작업 추가

---
+++ Source/SurvivorsRoguelike/Character/ProtoCharacter.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProtoCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+AProtoCharacter::AProtoCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set size for collision capsule
+	GetCapsuleComponent()->SetCapsuleHalfHeight(92.f);
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SKM_Quinn(
+		TEXT("/Script/Engine.SkeletalMesh'/Game/SurvivorsTemplate/Demo/Characters/Mannequins/Meshes/SKM_Quinn.SKM_Quinn'"));
+	if (SKM_Quinn.Succeeded())
+		GetMesh()->SetSkeletalMesh(SKM_Quinn.Object);
+
+	GetMesh()->SetRelativeLocation(FVector3d(0.0, 0.0, -92.0));
+	GetMesh()->SetRelativeRotation(FRotator3d(0.0, -90.0, 0.0));
+
+	static ConstructorHelpers::FClassFinder<UAnimInstance> ABP_Quinn(
+		TEXT("/Script/Engine.AnimBlueprint'/Game/2_HHS/Animation/ABP_AimJog.ABP_AimJog_C'"));
+	if (ABP_Quinn.Succeeded())
+		GetMesh()->SetAnimInstanceClass(ABP_Quinn.Class);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	mCameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	mCameraBoom->SetupAttachment(RootComponent);
+	mCameraBoom->SetRelativeRotation(FRotator3d(-35.0, 0.0, 0.0));
+	mCameraBoom->TargetArmLength = 2500.f;
+	mCameraBoom->bInheritRoll = false;
+	mCameraBoom->bInheritPitch = false;
+	mCameraBoom->bInheritYaw = false;
+
+	// Create a follow camera
+	mFollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	mFollowCamera->SetupAttachment(mCameraBoom);
+	mFollowCamera->FieldOfView = 45.f;
+}
+
+// Called when the game starts or when spawned
+void AProtoCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProtoCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Source/SurvivorsRoguelike/Character/ProtoCharacter.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ProtoCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class SURVIVORSROGUELIKE_API AProtoCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AProtoCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+// ÄÄÆ÷³ÍÆ®
+protected:
+	// Camera boom positioning the camera behind the character
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> mCameraBoom;
+
+	// Follow camera
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> mFollowCamera;
+};
