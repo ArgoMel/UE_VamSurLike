d99d2ee89126fd6e5add061a315de83f192f84fa
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2023-12-21 09:39:42 +0900

    데이터 테이블 수정

--- Source/SurvivorsRoguelike/Public/Character/BaseLobbyCharacter.h
+++ Source/SurvivorsRoguelike/Public/Character/BaseLobbyCharacter.h
@@ -27,6 +27,13 @@ protected:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Component")
 	TObjectPtr<UCameraComponent> m_Camera;
 
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Animation")
+	TObjectPtr<UBaseLobbyPlayerAnimInst> m_Anim;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Animation")
+	TObjectPtr<UAnimMontage> m_ProneToStand;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Animation")
+	TObjectPtr<UAnimMontage> m_StandToProne;
+
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Movement", meta = (AllowPrivateAccess = true))
 	float m_MoveForwardValue;
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Movement", meta = (AllowPrivateAccess = true))
@@ -75,6 +82,7 @@ protected:
 	void PlayerCrouch(const FInputActionValue& Value);
 	void Interaction();
 	void Walk();
+	void Prone(const FInputActionValue& Value);
 
 public:
 	bool GetIsADS()
